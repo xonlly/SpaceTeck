251c4c25db6f1f9dad854377e31068afd664897c
xonlly <ariege.creation@gmail.com>
2015-03-18 22:07:41 +0100

    Revert "Test Bot Direction."
    This reverts commit 4186954b83bc78977c25a0f6b06b40b5a48fb44b.

--- scripts_server/IA.js
+++ scripts_server/IA.js
@@ -16,25 +16,6 @@ module.exports = {
 		}
 	},
 
-	trajectoires : {
-		config : {
-			limitsRadius : 150,
-		},
-		bots : {
-			'kbot1' : {
-				current : 0,
-				max : 4,
-				data : [
-					{ x : 0, y : 0 },
-					{ x : -1000, y: -1000 },
-					{ x : 1000, y: -1000 },
-					{ x : 1000, y: 1000 },
-					{ x : -1000, y: 1000 },
-				]
-			},
-		},
-	},
-
 	set : function (obj) {
 		this.config.obj.players = obj.players;
 		this.config.obj.io 		= obj.io;
@@ -56,7 +37,7 @@ module.exports = {
 		setInterval(function () {
 			this.draw();
 			if (this.config.debug) { console.log(Players.config.players[this.config.bot.keyLabel+i]); }
-		}.bind(this), 1000 / 40);
+		}.bind(this), 1000);
 
 		return this;
 	},
@@ -73,32 +54,6 @@ module.exports = {
 	draw : function () {
 		var Players = this.config.obj.players;
 		for (var i = 1; i <= this.config.bot.nb; i++) {
-			// Déplacements:
-			// console.log(this.config.bot.keyLabel+i);
-			var botDeplacements = this.trajectoires.bots[this.config.bot.keyLabel+i];
-			if (this.Math.getDistanceElements(
-				botDeplacements.data[botDeplacements.current].x, 
-				botDeplacements.data[botDeplacements.current].y, 
-				Players.config.players[this.config.bot.keyLabel+i].x, 
-				Players.config.players[this.config.bot.keyLabel+i].y
-				) < this.trajectoires.config.limitsRadius) {
-
-				console.log(botDeplacements.current +'>='+ botDeplacements.max);
-				this.trajectoires.bots[this.config.bot.keyLabel+i].current = botDeplacements.current >= botDeplacements.max ? 0 : botDeplacements.current+1;
-
-			}
-
-			Players.config.players[this.config.bot.keyLabel+i].vitesse = 5;
-			Players.config.players[this.config.bot.keyLabel+i].mouse.orientation = Players.config.players[this.config.bot.keyLabel+i].orientation = this.Math.getAngleByPositions(
-				Players.config.players[this.config.bot.keyLabel+i].x, 
-				Players.config.players[this.config.bot.keyLabel+i].y,
-				botDeplacements.data[botDeplacements.current].x, 
-				botDeplacements.data[botDeplacements.current].y
-				);
-
-			//console.log(Players.config.players[this.config.bot.keyLabel+i].x + ' - ' + Players.config.players[this.config.bot.keyLabel+i].y);
-
-			/*
 			if (Math.floor((Math.random() * 2)+1) == 2) {
 				Players.config.players[this.config.bot.keyLabel+i].vitesse += Players.config.players[this.config.bot.keyLabel+i].vitesse > 15 ? 15 : Math.floor((Math.random() * 2)+1);
 				Players.config.players[this.config.bot.keyLabel+i].mouse.orientation = Players.config.players[this.config.bot.keyLabel+i].orientation += Math.floor((Math.random() * 35) + 1);;
@@ -109,28 +64,9 @@ module.exports = {
 
 			if (Math.floor((Math.random() * 2)+1) == 2) {
 				//GameObject.Bullet.set(btName+i);
-			}*/
-
+			}
 			Players.update(i, Players.config.players[this.config.bot.keyLabel+i]);
 			this.config.obj.io.emit('player', { pid: this.config.bot.keyLabel+i, donnees : Players.config.players[this.config.bot.keyLabel+i] });
 		}
-	},
-
-	Math :
-	{
-		getAngleByPositions : function (x, y, x2, y2)
-		{
-			var dy = y - y2;
-			var dx = x - x2;
-			var intReturn = Math.atan2(dy, dx);
-			intReturn *= 180/Math.PI // rads to degs
-			return intReturn;
-		},
-
-		getDistanceElements : function(x, y, x2, y2)
-		{
-			return Math.sqrt(Math.pow((x - x2), 2) + Math.pow((y - y2), 2));
-		},
-
-	},
+	}
 };
--- scripts_server/Players.js
+++ scripts_server/Players.js
@@ -24,7 +24,7 @@ module.exports = {
 
 		setInterval(function () {
 			this.refresh();
-		}.bind(this), 1000 / 40);
+		}.bind(this), 40);
 
 		return this;
 	},
@@ -34,155 +34,12 @@ module.exports = {
 		for (var key in this.config.players) {
 			this.config.players[key].x += Math.cos((this.config.players[key].orientation)*Math.PI/180) * -this.config.players[key].vitesse;
 			this.config.players[key].y += Math.sin((this.config.players[key].orientation)*Math.PI/180) * -this.config.players[key].vitesse;
-
-
-			this.config.players[key].vitesse = this.Physicx.getVitesseByOrientation(
-				this.config.players[key].orientation, this.config.players[key].orientation, this.config.players[key].vitesse
-				);
-
 		}
 	},
 
 	getAll : function ()
 	{
 		return this.config.players;
-	},
-
-	Physicx :
-	{
-		isColision : function (x, y, objectSize, playerid)
-		{
-			elementSize = objectSize == undefined ? 0 : objectSize;
-			playerid = playerid == undefined ? GameObject.Player.config.me : playerid;
-			// Colision avec des items autres:
-			for (var key in listItems) {
-				if (listItems[key].isMe) {
-					if (GameObject.Player.isActive(key)) { continue; }
-				}
-
-				if (listItems[key].isMe == playerid) {
-					continue;
-				}
-
-				shipDist = Math.sqrt(Math.pow((listItems[key].x - x), 2) + Math.pow((listItems[key].y - y), 2));
-				if (shipDist - elementSize < listItems[key].distance && shipDist != 0) {
-					return key;
-				}
-
-			}
-
-			// Colision avec la map:
-			for (var key in GameObject.MapServeur) {
-				var nWidth = (imagesLoaded[GameObject.MapServeur[key].imageName].image.width)/2;
-				var nHeight = (imagesLoaded[GameObject.MapServeur[key].imageName].image.height)/2;
-
-				shipDist = Math.sqrt(Math.pow((GameObject.MapServeur[key].x - x), 2) + Math.pow((GameObject.MapServeur[key].y - y), 2));
-				if (shipDist < nWidth + 30 && shipDist != 0) {
-					var imageMapped = imagesMapping[GameObject.MapServeur[key].imageName];
-					for (var keyMap in imageMapped) {
-						shipDistMap = Math.sqrt(Math.pow((GameObject.MapServeur[key].x - nWidth + imageMapped[keyMap].x - x), 2) + Math.pow((GameObject.MapServeur[key].y - nHeight + imageMapped[keyMap].y - y), 2));
-						if (shipDistMap - elementSize < 10 && shipDistMap != 0) {
-							return key;
-						}
-					}
-				}
-			}
-		},
-
-		getGravityEffect : function (x, y, vitesse, orientation)
-		{
-			for (var key in listItems) {
-
-				if (listItems[key].isMe) {
-					if (GameObject.Player.isActive(key)) { continue; }
-				}
-
-				if (listItems[key].isMe == GameObject.Player.config.me) {
-					continue;
-				}
-				if (!listItems[key].gravity) {
-					continue;
-				}
-
-				shipDist = Math.sqrt(Math.pow((listItems[key].x - x), 2) + Math.pow((listItems[key].y - y), 2));
-				orentationZone = GameObject.Math.getAngleByPositions(x, y, listItems[key].x, listItems[key].y)/0.5;
-
-				if (shipDist - elementSize < listItems[key].distance+listItems[key].gravityRadius && shipDist != 0) {
-					//vitesse ++;
-					//force = (((shipDist-listItems[key].distance)/listItems[key].gravityRadius)*1.9)*4;
-					force = 5;
-					newOrientation = orientation - orentationZone / 23;
-					x +=
-						(Math.cos((orientation)*Math.PI/180) +
-						Math.cos((orentationZone)*Math.PI/180)/force)/2 *
-						-vitesse;z
-
-					y +=
-						(Math.sin((orientation)*Math.PI/180) +
-						Math.sin((orentationZone)*Math.PI/180)/force)/2 *
-						-vitesse;
-
-					vitesse = GameObject.Physicx.getVitesseByOrientation(
-						orientation, newOrientation, vitesse
-						);
-
-					return {
-						x: x,
-						y: y,
-						orientation: newOrientation,
-						vitesse: vitesse
-					};
-				}
-
-			}z
-
-			return {
-				x: x,
-				y: y,
-				orientation: orientation,
-				vitesse: vitesse
-			};
-		},
-
-		getVitesseByOrientation : function (lastOrientation, newOrientation, vitesse)
-		{
-			if (lastOrientation == newOrientation) { // Aucun changement.
-				return vitesse;
-			}
-
-			if (lastOrientation != 0) {
-				rotationLevel = Math.abs(((newOrientation)*Math.PI/180) - (lastOrientation)*Math.PI/180)
-
-				if (rotationLevel > 180) {
-					rotationLevel -= 180;
-				}
-
-				newSpeed = vitesse - ((Math.abs(rotationLevel) / 180) * vitesse)*1;
-
-				return newSpeed > 0 ? newSpeed : 0;
-			}
-			return vitesse;
-		},
-		addVect : function (obj, angle, force)
-        {
-
-            /*var vectR = {angle : 0, vitesse: 0};
-            vectR.vitesse = Math.sqrt(obj.vitesse*obj.vitesse + force*force - 2*obj.vitesse*force * Math.cos(angle));
-            vectR.vitesse = obj.vitesse;
-            console.log((obj.orientation - obj.mouse.orientation));
-
-            var a = obj.mouse.orientation - obj.orientation;
-			a += (a>180) ? -360 : (a<-180) ? 360 : 0;
-
-            var fdsvjiop = (obj.vitesse * obj.vitesse) + (vectR.vitesse*vectR.vitesse) - (2*obj.vitesse*vectR.vitesse) * Math.cos(a);
-
-            console.log(fdsvjiop);
-            
-            //vectR.angle = Math.cos(((obj.vitesse * obj.vitesse) + (vectR.vitesse*vectR.vitesse) - (fdsvjiop*fdsvjiop)) / (2*obj.vitesse*vectR.vitesse));
-            vectR.angle = Math.cos(obj.vitesse * obj.vitesse + vectR.vitesse*vectR.vitesse - fdsvjiop*fdsvjiop / 2*obj.vitesse*vectR.vitesse) * Math.PI / 180;
-            return vectR;*/
-        },
-
 	},
 
 	set : function (id, spawn)
