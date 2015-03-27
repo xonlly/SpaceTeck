ad63f1efe47e173a29b4a2af5036fda9de7350c4
xonlly <ariege.creation@gmail.com>
2015-03-27 23:38:29 +0100

    Debug pour la génération de la map

--- server.js
+++ server.js
@@ -4,7 +4,14 @@
 var _server = require('./scripts_server/Server.js')();
 
 _server.init();
-//return;
+
+if (process.argv[2] == 'gen') {
+  setTimeout(function () {
+    console.log('Finish ! Please restart script.');
+  }, 6000);
+  return;
+}
+
 setTimeout(function () { _server.webServer(); }, 0); // Start du WebServer
 setTimeout(function () { _server.ioServer(); }, 0); // Stard du serveur IO.
 setTimeout(function () { _server.mapServer(); }, 0); // Stard du serveur IO.
